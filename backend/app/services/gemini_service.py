f6316b862b3d9b4d0502c1d58c973711d1422ae5
Rawlo Dudhisty <rawlodudhisty@gmail.com>
2026-06-22 14:30:14 +0530

    database chnages

--- backend/app/services/gemini_service.py
+++ backend/app/services/gemini_service.py
@@ -4,10 +4,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+print("GEMINI_API_KEY exists:", bool(os.getenv("GEMINI_API_KEY")))
+
 genai.configure(
     api_key=os.getenv("GEMINI_API_KEY")
 )
 
-model = genai.GenerativeModel(
-    "gemini-2.5-flash"
-)
+model = genai.GenerativeModel("gemini-2.5-flash")
